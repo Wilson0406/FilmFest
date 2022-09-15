a706a582cfddf401942ae70708280db508f3bbe4
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-09-15 13:24:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import React from "react";
+import * as ReactDOMClient from 'react-dom/client';
 
+import App from './App';
+
+
+const container = document.getElementById('root');
+
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
